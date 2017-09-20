c6b3e6ba6c49f3fcc6d125862936e441704c2bd2
Brandon Spruth <bspruth@gmail.com>
2017-09-20 12:22:21 -0400

    added matt to contributors
    thanks for the PR!

--- fortifyapi/fortify.py
+++ fortifyapi/fortify.py
@@ -2,7 +2,7 @@
 
 __author__ = "Brandon Spruth (brandon.spruth2@target.com), Jim Nelson (jim.nelson2@target.com)"
 __copyright__ = "(C) 2017 Target Brands, Inc."
-__contributors__ = ["Brandon Spruth", "Jim Nelson"]
+__contributors__ = ["Brandon Spruth", "Jim Nelson", "Matthew Dunaj"]
 __status__ = "Production"
 __license__ = "MIT"
 
