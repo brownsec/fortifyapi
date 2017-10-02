80daeb0d0c0fbbade1513cbecf49372fb5ce021c
Brandon Spruth <bspruth@gmail.com>
2017-10-02 09:35:49 -0500

    removed test/debug print statements

--- fortifyapi/fortify.py
+++ fortifyapi/fortify.py
@@ -374,17 +374,7 @@ class FortifyApi(object):
         :return: Response from the file upload operation
         """
         file_token = self.get_file_token('UPLOAD').data['data']['token']
-
         url = "/ssc/upload/resultFileUpload.html?mat=" + file_token
-
-        #test ntpath handling for codecs
-        print("isabs => {}".format(ntpath.isabs(file_path)))
-        print("dirname => {}".format(ntpath.dirname(file_path)))
-        print("basename => {}".format(ntpath.basename(file_path)))
-        print("normpath => {}".format(ntpath.normpath(file_path)))
-        print("split => {}".format(ntpath.split(file_path)))
-        print("join=> {}".format(ntpath.join(file_path, "TEST!!")))
-
         files = {'file': (ntpath.basename(file_path), open(file_path), 'rb')}
 
         headers = {
