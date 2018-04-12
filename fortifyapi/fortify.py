18ce25eb0dd082dd3017f27162740cb29f7bda85
Brandon Spruth <bspruth@gmail.com>
2018-04-11 21:09:24 -0500

    Hotfix fortify context url (#18)
    * removed /ssc context with uri and url values
    * removed unused urllib module
    * revd up
    * remove debug statement

--- fortifyapi/fortify.py
+++ fortifyapi/fortify.py
@@ -66,7 +66,6 @@ class FortifyApi(object):
                                                                  business_risk_ranking=business_risk_ranking,
                                                                  custom_attribute=custom_attribute)
         url = '/api/v1/bulk'
-        print(data)
         return self._request('POST', url, data=data)
 
     @staticmethod
